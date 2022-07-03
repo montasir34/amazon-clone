4e14eff478587ff5e1ae447223869d7786d9a38e
montasir34 <mntsr9999.z@gmail.com>
2022-07-03 07:25:32 -0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1 className='text-5xl text-red-600'>helo</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ4RnzT-je104g33QqEz1cdei02yNAd7c",
+  authDomain: "clone-800f5.firebaseapp.com",
+  projectId: "clone-800f5",
+  storageBucket: "clone-800f5.appspot.com",
+  messagingSenderId: "571783409428",
+  appId: "1:571783409428:web:a14a93a2664239d95b1c01"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
